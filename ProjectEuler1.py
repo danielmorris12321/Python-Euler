8a793fb76eb357f2d4508f0ab68d0c7bbf7bea89
Daniel <danny_morris_3000@hotmail.com>
2019-12-26 12:35:22 +0000

    Project Euler 1 automated test

--- ProjectEuler1.py
+++ ProjectEuler1.py
@@ -4,9 +4,16 @@ Created on Thu Dec 26 10:52:25 2019
 
 @author: Daniel
 """
-sum = 0
 
-for i in range(1,1000):
-    if(i % 3  == 0) or (i % 5 == 0):
-        sum = sum + i    
-print(sum)
+def test_sumCounter():
+    assert sumCounter(10) == 23, "Sum of natural numbers below 10 multiples of 3 and 5 should be 23"
+
+def sumCounter(limit):
+    sum = 0
+    for i in range(1,limit):
+        if(i % 3  == 0) or (i % 5 == 0):
+            sum = sum + i    
+    return(sum)
+
+test_sumCounter()
+print(sumCounter(1000))    
